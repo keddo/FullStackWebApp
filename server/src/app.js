66c9a32d87db499f41b39372d0dffefb72ce4e6d
= <=>
2019-08-16 17:17:28 +0300

    Set up the express JS Server

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const PORT =  5000;
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+app.get('/status', (req, res) => {
+    res.send({message: "Hello world!"});
+})
+app.listen(PORT, () => {
+    console.log(`Server running on Port: ${PORT}`);
+});
+

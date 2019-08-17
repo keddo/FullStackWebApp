7c61c1cc1b5799d4c254d9bb678ee059e1e16754
= <=>
2019-08-17 12:08:59 +0300

    Setting up the registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" v-model="email" name="email" placeholder="email"><br><br>
+    <input type="password" v-model="password" name="password" placeholder="password"><br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationServices'
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
-app.get('/status', (req, res) => {
-    res.send({message: "Hello world!"});
-})
+app.post('/register', (req, res) => {
+    res.send({message: `Hello ${req.body.email} Your User was registered! Have Fun!`});
+});
 app.listen(PORT, () => {
     console.log(`Server running on Port: ${PORT}`);
 });
